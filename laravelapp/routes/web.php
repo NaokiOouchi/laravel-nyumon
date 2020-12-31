e557bbc618bb3bd25e58b84130d3d946c7d8e334
OouchiNaoki <noouchi4@gmail.com>
2021-01-01 03:57:20 +0900

    No.59 コントローラーに複数アクションを利用する

--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 });
 
 Route::get('hello', 'HelloController@index');
+Route::get('hello/other', 'HelloController@other');
 
 // Route::get('hello/{msg?}', function ($msg ="no message.") {
 //     $html = <<<EOF
