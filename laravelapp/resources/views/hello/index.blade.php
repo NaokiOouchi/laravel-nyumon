6b37379a7e15c10a8d7cb61964f0abe5b0aee4ca
OouchiNaoki <noouchi4@gmail.com>
2021-01-02 16:19:11 +0900

    No.103 レイアウトの作成 @sectionは@showまでで区切られる

--- laravelapp/resources/views/hello/index.blade.php
+++ laravelapp/resources/views/hello/index.blade.php
@@ -1,25 +1,15 @@
-<html>
-    <head>
-    <title>Hello/Index</title>
-    <style>
-    body {font-size:16pt; color:#999;}
-    h1 {font-size:100pt; text-align:right; color:#eee;
-        margin:-40px 0px -50px 0px;}
-    </style>
-    </head>
-    <body>
-        <h1>Blade/Index</h1>
-        <p>&#064;whileディレクティブの例</p>
-        <ol>
-            @php
-                $counter = 0;
-            @endphp
-            @while ($counter < count($data))
-                <li>{{$data[$counter]}}</li>
-                @php
-                    $counter++;
-                @endphp
-            @endwhile
-        </ol>
-    </body>
-</html>
+@extends('layouts.helloapp')
+@section('title')
+    Index
+@endsection
+    @section('menubar')
+        @parent
+        インデックスページ
+    @endsection
+    @section('content')
+        <p>ここが本文のコンテンツです。</p>
+        <p>好きなだけ記述できます。</p>
+    @endsection
+    @section('footer')
+        copyright 2020 tuyano.
+    @endsection
