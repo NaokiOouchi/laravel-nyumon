2c1c584fe7c87edf6a6d9228a6a05fd5c17f9498
OouchiNaoki <noouchi4@gmail.com>
2021-01-02 16:27:58 +0900

    No.105 コンポーネントの作成 @slotで変数に値を代入できる

--- laravelapp/resources/views/hello/index.blade.php
+++ laravelapp/resources/views/hello/index.blade.php
@@ -9,6 +9,14 @@
     @section('content')
         <p>ここが本文のコンテンツです。</p>
         <p>好きなだけ記述できます。</p>
+        @component('components.message')
+            @slot('msg_title')
+                CAUTION!
+            @endslot
+            @slot('msg_content')
+                これはメッセージの表示です。
+            @endslot
+        @endcomponent
     @endsection
     @section('footer')
         copyright 2020 tuyano.
