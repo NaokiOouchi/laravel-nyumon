bbf745e66241d8c5802a24cf76f42c512d3fde9a
OouchiNaoki <noouchi4@gmail.com>
2021-01-03 02:46:36 +0900

    No.235 クエリビルダー

--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -26,6 +26,7 @@ Route::get('hello/edit', 'HelloController@edit');
 Route::post('hello/edit', 'HelloController@update');
 Route::get('hello/del', 'HelloController@del');
 Route::post('hello/del', 'HelloController@remove');
+Route::get('hello/show', 'HelloController@show');
 
 // Route::get('hello/{msg?}', function ($msg ="no message.") {
 //     $html = <<<EOF
