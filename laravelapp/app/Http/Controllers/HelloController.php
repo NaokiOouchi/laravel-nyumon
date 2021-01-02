32b1132f3b8db4c8c4de89d2d87e8e727054d074
OouchiNaoki <noouchi4@gmail.com>
2021-01-02 23:00:15 +0900

    No.179 cookieの登録

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -9,10 +9,22 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        return view("hello.index", ["msg"=>"フォームを入力ください。"]);
+        if ($request->hasCookie("msg")) {
+            $msg="Coolie:".$request->cookie("msg");
+        } else {
+            $msg="※クッキーはありません。";
+        }
+        return view("hello.index", ["msg"=>$msg]);
     }
-    public function post(HelloRequest $request)
+    public function post(Request $request)
     {
-        return view("hello.index", ["msg"=>"正しく入力されました！"]);
+        $validate_rule = [
+            "msg"=>"required",
+        ];
+        $this->validate($request, $validate_rule);
+        $msg = $request->msg;
+        $response = response()->view("hello.index", ["msg"=>$msg]);
+        $response->cookie("msg", $msg, 100);
+        return $response;
     }
 }
