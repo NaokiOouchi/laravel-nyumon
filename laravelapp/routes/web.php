77420a88e0441b7a7cd2c3ecd631371be88a8fbd
OouchiNaoki <noouchi4@gmail.com>
2021-01-03 02:28:57 +0900

    No.217 delete

--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -24,6 +24,8 @@ Route::get('hello/add', 'HelloController@add');
 Route::post('hello/add', 'HelloController@create');
 Route::get('hello/edit', 'HelloController@edit');
 Route::post('hello/edit', 'HelloController@update');
+Route::get('hello/del', 'HelloController@del');
+Route::post('hello/del', 'HelloController@remove');
 
 // Route::get('hello/{msg?}', function ($msg ="no message.") {
 //     $html = <<<EOF
