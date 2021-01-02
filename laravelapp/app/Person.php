ac496dc8c543e06c70d5fd31a1a8009c7917a028
OouchiNaoki <noouchi4@gmail.com>
2021-01-03 05:37:45 +0900

    No.277

--- laravelapp/app/Person.php
+++ laravelapp/app/Person.php
@@ -7,26 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
-    protected static function boot()
-    {
-        parent::boot();
-        static::addGlobalScope(new ScopePerson);
-    }
+    protected $guarded = array("id");
+    public static $rules = array(
+        "name"=>"required",
+        "mail"=>"email",
+        "age"=>"integer|min:0|max:150"
+    );
     public function getData()
     {
         # code...
         return $this->id.':'.$this->name.'('.$this->age.')';
-    }
-    public function scopeNameEqual($query, $str)
-    {
-        return $query->where("name", $str);
-    }
-    public function scopeAgeGreaterThan($query, $n)
-    {
-        return $query->where("age", ">=", $n);
-    }
-    public function scopeAgeLessThan($query, $n)
-    {
-        return $query->where("age", "<=", $n);
     }
 }
