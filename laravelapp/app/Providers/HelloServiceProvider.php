f5a60cd1a28a74bf8b3ec1b2c57bba60b2b165e5
OouchiNaoki <noouchi4@gmail.com>
2021-01-02 22:45:30 +0900

    No.169 Validator::extendを使うとシンプルにかける

--- laravelapp/app/Providers/HelloServiceProvider.php
+++ laravelapp/app/Providers/HelloServiceProvider.php
@@ -2,10 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 use Validator;
-use App\Http\Validators\HelloValidator;
 
 class HelloServiceProvider extends ServiceProvider
 {
@@ -26,9 +24,8 @@ class HelloServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $validator = $this->app["validator"];
-        $validator->resolver(function ($translator, $data, $rules, $messages) {
-            return new HelloValidator($translator, $data, $rules, $messages);
+        Validator::extend("hello", function ($translator, $value, $parameters, $validator) {
+            return $value % 2 ==0;
         });
     }
 }
