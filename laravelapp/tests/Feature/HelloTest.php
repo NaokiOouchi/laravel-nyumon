28c4560fe8d7d092c60b4b4e0cc5979750b815f6
OouchiNaoki <noouchi4@gmail.com>
2021-01-04 07:20:02 +0900

    No.370 データベーステスト

--- laravelapp/tests/Feature/HelloTest.php
+++ laravelapp/tests/Feature/HelloTest.php
@@ -2,12 +2,15 @@
 
 namespace Tests\Feature;
 
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class HelloTest extends TestCase
 {
+    use DatabaseMigrations;
     /**
      * A basic feature test example.
      *
@@ -16,11 +19,16 @@ class HelloTest extends TestCase
     public function testHello()
     {
         $this->assertTrue(true);
-        $arr=[];
-        $this->assertEmpty($arr);
-        $msg = "Hello";
-        $this->assertEquals("Hello", $msg);
-        $n = random_int(0, 100);
-        $this->assertLessThan(100, $n);
+        $response = $this->get("/");
+        $response->assertStatus(200);
+        $response = $this->get("/hello");
+        $response->assertStatus(302);
+
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)->get("/hello");
+        $response->assertStatus(200);
+
+        $response = $this->actingAs($user)->get("/no_route");
+        $response->assertStatus(404);
     }
 }
